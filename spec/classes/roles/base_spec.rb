b2a7535f82aba5cf25e646ed3bfae09b88d08d70
William DURAND <william.durand1@gmail.com>
2012-11-26 19:16:29 +0100

    Add  tests for base role

---
+++ spec/classes/roles/base_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe 'bazinga::roles::base', :type => :class do
+  let(:title) { 'bazinga::roles:base' }
+  let(:facts) {{ :osfamily => 'Debian' }}
+
+  it { should contain_class('apt') }
+  it { should contain_class('ntp') }
+
+  it { should contain_package('openssh-server').with_ensure('present') }
+  it { should contain_service('ssh').with_ensure('running') }
+
+  it { should contain_package('nfs-common').with_ensure('absent') }
+  it { should contain_package('portmap').with_ensure('absent') }
+
+  describe 'with vagrant = true' do
+    let(:params) {{ :vagrant => true }}
+
+    it { should contain_package('nfs-common').with_ensure('present') }
+    it { should contain_package('portmap').with_ensure('present') }
+  end
+end
