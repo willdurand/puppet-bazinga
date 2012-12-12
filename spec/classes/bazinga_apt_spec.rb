14f35886f9bd5fbb9cc2fc564ad4fcee7658f1bc
William DURAND <william.durand1@gmail.com>
2012-12-12 13:51:57 +0100

    Fix specs

--- spec/classes/bazinga_apt_spec.rb
+++ spec/classes/bazinga_apt_spec.rb
@@ -3,17 +3,27 @@ require 'spec_helper'
 describe 'bazinga::apt', :type => :class do
   let(:title) { 'bazinga::apt' }
 
-  describe 'with Debian as OS family' do
-    let(:facts) {{ :osfamily => 'Debian' }}
+  describe 'with Debian' do
+    let(:facts) {{ :operatingsystem => 'Debian' }}
 
     it { should contain_apt__source('debian') }
     it { should contain_apt__source('dotdeb') }
+    it { should_not contain_apt__source('ubuntu') }
   end
 
-  describe 'with another OS family' do
-    let(:facts) {{ :osfamily => 'Redhat' }}
+  describe 'with Ubuntu' do
+    let(:facts) {{ :operatingsystem => 'Ubuntu' }}
 
     it { should_not contain_apt__source('debian') }
     it { should_not contain_apt__source('dotdeb') }
+    it { should contain_apt__source('ubuntu') }
+  end
+
+  describe 'with another OS' do
+    let(:facts) {{ :operatingsystem => 'Redhat' }}
+
+    it { should_not contain_apt__source('debian') }
+    it { should_not contain_apt__source('dotdeb') }
+    it { should_not contain_apt__source('ubuntu') }
   end
 end
