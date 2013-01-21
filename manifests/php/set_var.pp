e5abd96c8ed722a994d0471a03cd7c161a3f578a
William DURAND <william.durand1@gmail.com>
2013-01-21 15:21:00 +0100

    Better fix for php::set_var

--- manifests/php/set_var.pp
+++ manifests/php/set_var.pp
@@ -6,20 +6,19 @@
 #   The new value
 #
 # [*file_ini*]
-#   The filename to update
+#   The file to update
 #
 define bazinga::php::set_var (
   $value,
-  $file_ini
+  $file_ini,
+  $notify = $::bazinga::roles::php::notify_service
 ) {
-
-  include php::params
 
   exec { "bazinga_php_set_var_${file_ini}_${name}":
     command => "sed -i 's/^;*[[:space:]]*${name}[[:space:]]*=.*$/${name} = ${value}/g' ${file_ini}",
     unless  => "grep -xqe '${name}[[:space:]]*=[[:space:]]*${value}' -- ${file_ini}",
     path    => '/bin:/usr/bin',
     require => Class['bazinga::roles::php'],
-    notify  => $::bazinga::roles::php::notify_service,
+    notify  => $notify,
   }
 }
--- spec/defines/bazinga_php_set_var_spec.rb
+++ spec/defines/bazinga_php_set_var_spec.rb
@@ -6,27 +6,13 @@ describe 'bazinga::php::set_var', :type => :define do
     :osfamily => 'Debian'
   } }
 
-  describe 'with name and value parameters' do
-    let(:params) { {
-      :name     => 'post_max_size',
-      :value    => '500M',
-      :file_ini => '/etc/php5/conf.d/default.ini'
-    } }
+  let(:params) { {
+    :name     => 'post_max_size',
+    :value    => '500M',
+    :file_ini => 'my_file.ini',
+  } }
 
-    #it { should contain_exec('bazinga_php_set_var_default.ini_post_max_size') \
-    #  .with_command("sed -i 's/^;*[[:space:]]*post_max_size[[:space:]]*=.*$/post_max_size = 500M/g' /etc/php5/conf.d/default.ini")
-    #}
-  end
-
-  describe 'with a given file name' do
-    let(:params) { {
-      :name     => 'post_max_size',
-      :value    => '500M',
-      :file_ini => 'my_file.ini',
-    } }
-
-    it { should contain_exec('bazinga_php_set_var_my_file.ini_post_max_size') \
-      .with_command("sed -i 's/^;*[[:space:]]*post_max_size[[:space:]]*=.*$/post_max_size = 500M/g' my_file.ini")
-    }
-  end
+  it { should contain_exec('bazinga_php_set_var_my_file.ini_post_max_size') \
+    .with_command("sed -i 's/^;*[[:space:]]*post_max_size[[:space:]]*=.*$/post_max_size = 500M/g' my_file.ini")
+  }
 end
